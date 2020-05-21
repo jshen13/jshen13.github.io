4cf461a2f4a3f434ad2125b16a18cb8fce7cdd53
Jeffrey Shen <jeffreyshen13@gmail.com>
2020-05-21 15:52:53 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function showDesc(x) {
+    var popup = document.getElementById(x);
+    popup.classList.toggle("show");
+}
+  
+
+function hideDesc(x) {
+    var popup = document.getElementById(x);
+    popup.classList.toggle("show");
+  }
