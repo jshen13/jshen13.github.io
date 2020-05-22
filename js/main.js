8e98c5fb31718e3b2fede092bada5d418ad8c03c
Jeffrey Shen <jeffreyshen13@gmail.com>
2020-05-21 17:47:52 -0700

    updated responsiveness

--- js/main.js
+++ js/main.js
@@ -7,4 +7,14 @@ function showDesc(x) {
 function hideDesc(x) {
     var popup = document.getElementById(x);
     popup.classList.toggle("show");
+}
+  
+
+function toggleNavBar() {
+    var x = document.getElementById("nav");
+    if (x.className === "nav-bar") {
+      x.className += " responsive";
+    } else {
+      x.className = "nav-bar";
+    }
   }
