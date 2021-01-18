c21f2cafa1e8fcaf6ff37406de1905fc096898bf
Jeffrey Shen <jeffreyshen13@gmail.com>
2021-01-17 23:11:39 -0800

    added google analytics

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -38,6 +38,15 @@ module.exports = {
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    `gatsby-plugin-offline`
+    `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "UA-151003054-2", 
+        ],
+      },
+    },
   ],
 }
