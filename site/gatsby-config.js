ce20513f6be02e1f0d0192dcef68cb0bd0f1ed6b
Jeffrey Shen <jeffreyshen13@gmail.com>
2025-11-11 22:41:31 -0800

    update google analytics to gatsby-plugin-google-gtag plugin

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -51,14 +51,26 @@ module.exports = {
     //   },
     // },
     {
-      resolve: `gatsby-plugin-gtag`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        // your google analytics tracking id
-        trackingIds: ["G-TJKYT54R24"],
-        // Puts tracking script in the head instead of the body
-        head: false,
-        // enable ip anonymization
-        anonymize: true,
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-TJKYT54R24	", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Delays processing pageview events on route update (in milliseconds)
+          delayOnRouteUpdate: 0,
+        },
       },
     },
     `gatsby-plugin-preload-fonts`
